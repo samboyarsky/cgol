3252482be68ac704f4e09bcd723b0fc457a5ff10
Sam Boyarsky <sam.boyarsky@Sams-MacBook-Pro.local>
2016-09-01 22:19:40 -0400

    updated click mechanism, added stop button

--- js/cgol.js
+++ js/cgol.js
@@ -1,125 +1,108 @@
 class Board{
     constructor(x, y) {
-        this.width = x || 10;
-        this.height = y || 10;
-        this.grid = this.buildGrid();
+        this.width = x || 10
+        this.height = y || 10
+        this.grid = this.buildGrid()
     }
+
     buildGrid() {
-        //var out = new Array(this.width);
         var out = []
         for(var x = 0; x < this.width; x++){
             out[out.length] = []
             for(var y = 0; y < this.height; y++){
-                out[x][y] = false;
+                out[x][y] = false
             }
         }
-        return out;
+        return out
     }
+
     neighborCount(x,y){
-        var count = 0;
-        if(x > 0            &&                      this.grid[x-1][y])   { count++; }
-        if(x > 0            && y > 0             && this.grid[x-1][y-1]) { count++; }
-        if(x > 0            && y < this.height-1 && this.grid[x-1][y+1]) { count++; }
-        if(x < this.width-1 &&                      this.grid[x+1][y])   { count++; }
-        if(x < this.width-1 && y > 0             && this.grid[x+1][y-1]) { count++; }
-        if(x < this.width-1 && y < this.height-1 && this.grid[x+1][y+1]) { count++; }
-        if(                    y > 0             && this.grid[x]  [y-1]) { count++; }
-        if(                    y < this.height-1 && this.grid[x]  [y+1]) { count++; }
+        var count = 0
+        if(x > 0            &&                      this.grid[x-1][y])   { count++ }
+        if(x > 0            && y > 0             && this.grid[x-1][y-1]) { count++ }
+        if(x > 0            && y < this.height-1 && this.grid[x-1][y+1]) { count++ }
+        if(x < this.width-1 &&                      this.grid[x+1][y])   { count++ }
+        if(x < this.width-1 && y > 0             && this.grid[x+1][y-1]) { count++ }
+        if(x < this.width-1 && y < this.height-1 && this.grid[x+1][y+1]) { count++ }
+        if(                    y > 0             && this.grid[x]  [y-1]) { count++ }
+        if(                    y < this.height-1 && this.grid[x]  [y+1]) { count++ }
         return count
     }
+
     update(){
-        var newGrid = this.buildGrid();
+        var newGrid = this.buildGrid()
         for(var x = 0; x < this.width; x++){
             for(var y = 0; y < this.height; y++){
                 // Any live cell with fewer than two live neighbors dies, as if caused by under-population.
                 // Any live cell with two or three live neighbors lives on to the next generation.
                 // Any live cell with more than three live neighbors dies, as if by overcrowding.
                 // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-                var nc = this.neighborCount(x,y);
-                if(this.grid[x][y] && (nc == 2 || nc ==3)){
-                    newGrid[x][y] = true;
-                }
-                else if (!this.grid[x][y] && nc == 3){
-                    newGrid[x][y] = true;
+                var nc = this.neighborCount(x,y)
+                var $curr = $('#cell_' + x + '_' + y)
+                if( (this.grid[x][y] && nc == 2) || nc ==3 ){
+                    newGrid[x][y] = true
+                    $curr.addClass('live')
                 }
                 else{
-                    newGrid[x][y] = false;
+                    newGrid[x][y] = false
+                    $curr.removeClass('live')
                 }
             }
         }
-        this.grid = newGrid;
+        this.grid = newGrid
     }
-    setLive(x, y){
-        this.grid[x][y] = true;
-    }
+
     print(){
         for(var x = 0; x < this.width; x++){
-            var line = "";
+            var line = ''
             for(var y = 0; y < this.height; y++){
-                line += this.grid[x][y] + " ";
+                line += this.grid[x][y] + ' '
             }
-            console.log(line + "\n");
+            console.log(line + '\n')
         }
     }
 }
 
-function clickCell(who){
-    //console.log(who.data.x)
-    //console.log(who.data.elem);
-    console.log(this.getAttribute('x'))
-    if(b.grid[who.data.x][who.data.y]){
-        b.grid[who.data.x][who.data.y] = false;
-        who.data.elem.removeClass('live');
+function clickCell(){
+    var x = this.getAttribute('x')
+    var y = this.getAttribute('y')
+    if(b.grid[x][y]){
+         $(this).removeClass('live')
     }
     else{
-        b.grid[who.data.x][who.data.y] = true;
-        who.data.elem.addClass('live');
+         $(this).addClass('live')
     }
-
+    b.grid[x][y] = !b.grid[x][y]
 }
 
 function buildTable(){
-    var $table = $('#game_of_life_grid');
-    $table.empty();
+    var $table = $('#game_of_life_grid')
+    // $table.empty()
     for(var x = 0; x < b.width; x++){
-        var $tr = $('<tr>');
-        $table.append($tr)
-        // $tr.appendTo($table)
+        var $tr = $('<tr>')
         for(var y = 0; y < b.height; y++){
-
-            var $td = $('<td>');
-            // $td.attr("x", x);
-            // $td.attr("y", y);
-            // $td.attr("id", 'cell' + x + '_' + y);
-            $td.click({ x:x, y:y, elem:$td }, clickCell);
-            // $td.click({}, clickCell);
-            if(b.grid[x][y]) { $td.addClass('live'); }
-            $table.append($td)
+            var $td = $('<td>')
+            $td.attr({x:x,y:y,id:'cell_' + x + '_' + y})
+            $td.click(clickCell);
+            $tr.append($td)
         }
+        $table.append($tr)
     }
 }
 
 function doUpdate(){
-    b.update();
-    //buildTable();
+    b.update()
 }
 
-function toad(){
-    b.setLive(3,3);
-    b.setLive(4,3);
-    b.setLive(5,3);
-    b.setLive(4,4);
-    b.setLive(5,4);
-    b.setLive(6,4);
+b = new Board(50, 50)
+buildTable()
 
-}
-function blinker(){
-    b.setLive(4,4);
-    b.setLive(4,5);
-    b.setLive(4,6);
-}
-
-b = new Board(50,50);
-//blinker();
-//toad();
-buildTable();
+var interval_id = null
+$('#goButton').click(function (){
+    if(!interval_id){
+        interval_id = setInterval(doUpdate, 500)
+    }
+})
+$('#stopButton').click(function (){
+    interval_id = clearTimeout(interval_id)
+})
